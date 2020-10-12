fba744b6a28882e9ac1e8b633c1de912815c6623
SahilM1998 <63091402+SahilM1998@users.noreply.github.com>
2020-10-12 13:50:32 +0530

    Added java files
    Added java file of how to create string and string buffer in java

---
+++ JeetS.java
@@ -0,0 +1,8 @@
+class JeetS {
+  public static void main(String[] args) {
+
+    String Jeet = "Hello! World";
+    System.out.println("The string is: " + Jeet);
+
+  }
+}
---
+++ JeetSB.java
@@ -0,0 +1,7 @@
+class JeetSB { 
+    public static void main(String[] args) 
+    { 
+        StringBuffer s = new StringBuffer("Jeet Gujarathi");  
+        System.out.println(s);
+    } 
+}
